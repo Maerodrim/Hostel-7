add354348f714b4faad4972b4ff81e1e864f1584
Maerodrim <stanislav.sergynin@gmail.com>
2020-08-07 12:24:22 +0400

    init rep

---
+++ src/main/java/com/ssau/Hostel7/Hostel7Application.java
@@ -0,0 +1,13 @@
+package com.ssau.Hostel7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hostel7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hostel7Application.class, args);
+	}
+
+}
